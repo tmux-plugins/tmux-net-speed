3ed49dafef8a5644f82cd332fd2036f6c98ad57a
Joey Geralnik <jgeralnik@gmail.com>
2021-02-06 19:45:06 +0200

    Cache results to prevent miscalculations
    The code assumes that the status bar will only be updated every
    status-interval seconds
    
    In practice status-interval is a maximum value, not a minimum value. So
    if the status bar is actually updated every second even though the
    configuration is for 5 seconds all of the calculations are off and the
    bitrate will be calculated as a fifth of the real rate.
    
    See https://github.com/tmux-plugins/tmux-cpu/issues/15 for a similar
    issue with a different plugin, except that here it is much worse because
    it actually leads to a WRONG RESULT and not just a faster update. This
    commit implements @BrainMaestro 's suggestion from that thread.

--- scripts/upload_speed.sh
+++ scripts/upload_speed.sh
@@ -13,12 +13,19 @@ main()
 {
     # TODO make configurable
     #local upload_file=$(get_tmux_option $UPLOAD_FILE)
-    local file=$UPLOAD_FILE
-    local old_val=$(read_file $file)
-    local new_val=$(sum_upload_speed)
+    if ! is_update_needed $UPLOAD_TIME_FILE; then
+        local vel=$(read_file $UPLOAD_CACHE_FILE)
+    else
+        local file=$UPLOAD_FILE
+        local old_val=$(read_file $file)
+        local new_val=$(sum_upload_speed)
 
-    write_file $file $new_val
-    local vel=$(get_velocity $new_val $old_val)
+        write_file $file $new_val
+        local vel=$(get_velocity $new_val $old_val)
+
+        write_file $UPLOAD_TIME_FILE $(date +%s)
+        write_file $UPLOAD_CACHE_FILE "$vel"
+    fi
 
     ## Format output
     local format=$(get_tmux_option @upload_speed_format "%s")
